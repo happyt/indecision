9901debf0c10ffdcb5ba7f06e50c469c81f532b3
ian morton <ian.happytuesdays@gmail.com>
2017-11-20 14:44:33 +0000

    Initial scss setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Appn from './components/Appn'
+import './styles/styles.scss'
 
 ReactDOM.render(<Appn />, document.getElementById("app"))
